6909dee46725a7e1f37eb590fbeef0f73a6bd9db
irishandyesiam <nathan.irish@gmail.com>
2022-08-24 14:24:18 -0500

    Setup admin + urls file

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . serializers import ProductSerializer
+from . models import Product
 
-# Create your views here.
+@api_view(['GET'])
+def products_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    
+    return Response(serializer.data)
