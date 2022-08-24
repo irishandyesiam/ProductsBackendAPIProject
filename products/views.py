b7ff4ed0a76c34c4f9ff041f39e1d7bc3089bb69
irishandyesiam <nathan.irish@gmail.com>
2022-08-24 15:27:00 -0500

    Edited status codes.

--- products/views.py
+++ products/views.py
@@ -23,12 +23,12 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'DELETE':
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)    
